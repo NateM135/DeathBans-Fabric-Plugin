cd2136aa34e188ce8f2760391316212f2c55db6c
NateM135 <melwaninate@gmail.com>
2023-12-20 11:06:19 -0800

    update ban logic

--- src/main/java/com/natem135/deathbans/DeathBansServer.java
+++ src/main/java/com/natem135/deathbans/DeathBansServer.java
@@ -25,27 +25,24 @@ public class DeathBansServer implements DedicatedServerModInitializer {
         PlayerDeathCallback.EVENT.register((player, source) -> {
             LOGGER.info(String.format("[+] %s has Died.", player.getName()));
 
-            // Ten Minutes
+            // Create appropriate time objects
             int banDurationMillis = 60*1000;
-
             Date banStartTime = new Date();
             Date banExpirationTime = new Date(banStartTime.getTime() + banDurationMillis);
 
+            // Ban the user
             BannedPlayerList bannedPlayerList = Objects.requireNonNull(player.getServer()).getPlayerManager().getUserBanList();
+            BannedPlayerEntry bannedPlayerEntry = new BannedPlayerEntry(
+                    player.getGameProfile(),
+                    banStartTime,
+                    null,
+                    banExpirationTime,
+                    "You are currently death-banned."
+            );
+            bannedPlayerList.add(bannedPlayerEntry);
+            ServerPlayerEntity serverPlayerEntity = player.getServer().getPlayerManager().getPlayer(player.getGameProfile().getId());
 
-            // Ensure player is not already banned before banning.
-            if (!bannedPlayerList.contains(player.getGameProfile())) {
-                BannedPlayerEntry bannedPlayerEntry = new BannedPlayerEntry(
-                        player.getGameProfile(),
-                        banStartTime,
-                        null,
-                        banExpirationTime,
-                        "You are currently death-banned."
-                );
-                bannedPlayerList.add(bannedPlayerEntry);
-                ServerPlayerEntity serverPlayerEntity = player.getServer().getPlayerManager().getPlayer(player.getGameProfile().getId());
-            }
-
+            // Adding a ban entry does not remove the user, so remove the user.
             player.networkHandler.disconnect(Text.of(DeathMessageGenerator.getDeathMessage()));
         });
     }
