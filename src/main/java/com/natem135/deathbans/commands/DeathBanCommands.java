39fb5f3649009b5b1807ccdf5ddf2e117131c861
NateM135 <melwaninate@gmail.com>
2024-01-08 17:44:08 -0800

    Added two simple server-side commands.

--- src/main/java/com/natem135/deathbans/commands/DeathBanCommands.java
+++ src/main/java/com/natem135/deathbans/commands/DeathBanCommands.java
@@ -2,22 +2,30 @@ package com.natem135.deathbans.commands;
 
 import net.minecraft.server.command.CommandManager;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import com.natem135.deathbans.DeathBans;
+import com.mojang.brigadier.context.CommandContext;
 
 public class DeathBanCommands {
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(CommandManager.literal(DeathBans.MOD_ID)
                 .then(CommandManager.literal("status")
-                    .executes(context -> {
-                        context.getSource().sendFeedback(() -> Text.literal("The plugin is currently enabled!"), false);
-                        return 1;
-                }))
-                .then(CommandManager.literal("info")
-                .executes(context -> {
-                    context.getSource().sendFeedback(() -> Text.literal("This plugin was developed by NateM135."), false);
-                    return 1;
-                }))
+                    .executes(DeathBanCommands::getPluginStatus)
+                )
+                .then(CommandManager.literal("author")
+                    .executes(DeathBanCommands::getPluginInfo)
+                )
         ));
+    }
+
+    private static int getPluginStatus(CommandContext<ServerCommandSource> ctx) {
+        ctx.getSource().sendFeedback(() -> Text.literal("The plugin is currently enabled!"), false);
+        return 1;
+    }
+
+    private static int getPluginInfo(CommandContext<ServerCommandSource> ctx) {
+        ctx.getSource().sendFeedback(() -> Text.literal("The plugin was developed by NateM135."), false);
+        return 1;
     }
 }
