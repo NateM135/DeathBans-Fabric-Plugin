47d572d1f9e79aa2bfb3067f74d9bbacfaa26f68
NateM135 <melwaninate@gmail.com>
2024-01-11 15:32:23 -0800

    remove excess logging message

--- src/main/java/com/natem135/deathbans/mixin/PlayerManagerMixin.java
+++ src/main/java/com/natem135/deathbans/mixin/PlayerManagerMixin.java
@@ -1,15 +1,12 @@
 package com.natem135.deathbans.mixin;
 
 import net.minecraft.server.BannedPlayerEntry;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import com.mojang.authlib.GameProfile;
 import java.net.SocketAddress;
-import java.util.Arrays;
 
 import net.minecraft.server.PlayerManager;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -18,7 +15,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class PlayerManagerMixin {
     @Inject(method = "checkCanJoin", at = @At("HEAD"))
     private void checkCanJoin(SocketAddress address, GameProfile profile, CallbackInfoReturnable<Text> cir) {
-        System.out.println("checkCanJoinMixinHead");
         PlayerManager thisObject = (PlayerManager)(Object)this;
         BannedPlayerEntry entry = thisObject.getUserBanList().get(profile);
     }
